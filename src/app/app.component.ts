afd672fe4a46412fe96bc9a9dd99f05cb6bac557
sfresnel <sylvain@fresnel.me>
2024-02-05 10:27:31 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListeTachesComponent } from './liste-taches/liste-taches.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, ListeTachesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
